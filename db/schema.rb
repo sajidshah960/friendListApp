1da06a6f9b9a217573186e5a6281c2165a0b17fa
Sajid Shah <sajid.shah@devsinc.com>
2022-07-15 12:09:34 +0500

    temp

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+<<<<<<< Updated upstream
 ActiveRecord::Schema[7.0].define(version: 2022_06_09_133747) do
+=======
+ActiveRecord::Schema[7.0].define(version: 2022_07_15_070353) do
+  create_table "admins", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+>>>>>>> Stashed changes
   create_table "friends", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
@@ -27,4 +38,19 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_09_133747) do
     t.datetime "updated_at", null: false
   end
 
+<<<<<<< Updated upstream
+=======
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+>>>>>>> Stashed changes
 end
